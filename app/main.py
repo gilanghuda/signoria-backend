8a2b6c47c5b23e8b86e5d93d7d2764d0d0e39a53
gilanghuda <gilanghuda99@gmail.com>
2025-11-24 16:49:27 +0700

    fix: cors

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://signoria.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
